aae8d6ecf64d15ae9dccdd174a778d63454baf13
BradleySchapf <bradleyschapf@icloud.com>
2025-01-31 20:33:36 +0100

    changed folder structure

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header class="w-full mb-4">
+      <nav class="p-4">
+        <RouterLink to="/" class="mr-4">Home</RouterLink>
+        <RouterLink to="/blog">Blog</RouterLink>
+      </nav>
+    </header>
+
+    <main class="flex-1">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
