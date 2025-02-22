f9bcd5c2b428659e396610ba133736183bcbf29b
BradleySchapf <bradleyschapf@icloud.com>
2025-02-22 22:27:59 +0100

    updated styling menubar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,8 @@
 <template>
     <header class="w-full mb-4">
-        <nav class="p-4">
+        <nav class="container mx-auto p-4">
             <RouterLink to="/" class="mr-4">Home</RouterLink>
-      <RouterLink to="/blog">Blog</RouterLink>
+      <RouterLink to="/blog" class="mr-4">Blog</RouterLink>
       <RouterLink to="/portfolio">Portfolio</RouterLink>
     </nav>
   </header>
