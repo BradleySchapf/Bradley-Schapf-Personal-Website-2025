0eee29d79ca6e2ce8c25f762af89b675875478ec
BradleySchapf <bradleyschapf@icloud.com>
2025-02-23 21:14:02 +0100

    Finished Footer V1

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="w-full bg-gray-50">
-    <div class="container mx-auto p-4 flex justify-center items-center gap-x-4">
-      <a href="https://www.linkedin.com/in/bradley-schapf/" target="_blank">LinkedIn</a>
-      <a href="https://github.com/bradley-schapf" target="_blank">GitHub</a>
+  <footer class="w-full bg-gray-100">
+    <div class="container mx-auto py-2 flex justify-center items-center">
+      <a href="https://www.linkedin.com/in/bradley-schapf/" target="_blank" class="nav-link border-r border-gray-200">LinkedIn</a>
+      <a href="https://github.com/bradleyschapf" target="_blank" class="nav-link">GitHub</a>
     </div>
   </footer>
 </template>
