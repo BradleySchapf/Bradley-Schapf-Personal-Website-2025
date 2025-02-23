e1eecd03381b100650e853212144ac3108e41669
BradleySchapf <bradleyschapf@icloud.com>
2025-02-23 21:03:16 +0100

    finished header v1

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,15 +1,11 @@
 <template>
     <header class="w-full mb-4 bg-gray-100 ">
-      <nav class="container mx-auto p-4 flex justify-center items-center gap-x-12 hover:bg-gray-200">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/blog">Blog</RouterLink>
-        <RouterLink to="/portfolio">Portfolio</RouterLink>
+      <nav class="container mx-auto flex justify-center items-center py-2">
+        <RouterLink to="/" class="nav-link border-r border-gray-200">Home</RouterLink>
+        <RouterLink to="/blog" class="nav-link border-r border-gray-200">Blog</RouterLink>
+        <RouterLink to="/portfolio" class="nav-link">Portfolio</RouterLink>
     </nav>
   </header>
 </template>
 
-<style scoped>
-.menu-bar:hover {
-  background-color: #f0f0f0;
-}
-</style>
+
