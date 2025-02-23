4a58df787426b6d5995fb656268cabe64ca03699
BradleySchapf <bradleyschapf@icloud.com>
2025-02-23 20:17:48 +0100

    created basic footer, added minimum height

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MenuBar from './components/SiteHeader.vue'
+import SiteFooter from './components/SiteFooter.vue'
 </script>
 
 <template>
   <div class="flex flex-col h-full items-center justify-center text-center">
     <MenuBar />
-    <main class="flex-1 container mx-auto px-4">
+    <main class="flex-1 container mx-auto min-h-[750px]">
       <RouterView />
     </main>
+    <SiteFooter class="flex mt-auto"/>
   </div>
 </template>
 
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="w-full bg-gray-50">
+    <div class="container mx-auto p-4 flex justify-center items-center gap-x-4">
+      <a href="https://www.linkedin.com/in/bradley-schapf/" target="_blank">LinkedIn</a>
+      <a href="https://github.com/bradley-schapf" target="_blank">GitHub</a>
+    </div>
+  </footer>
+</template>
