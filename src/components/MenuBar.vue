712e4b1fe961a5b46ab70013de61f36b20d82025
BradleySchapf <bradleyschapf@icloud.com>
2025-02-23 15:24:29 +0100

    updated header style

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,9 +1,15 @@
 <template>
-    <header class="w-full mb-4">
-        <nav class="container mx-auto p-4">
-            <RouterLink to="/" class="mr-4">Home</RouterLink>
-      <RouterLink to="/blog" class="mr-4">Blog</RouterLink>
-      <RouterLink to="/portfolio">Portfolio</RouterLink>
+    <header class="w-full mb-4 bg-gray-100 ">
+      <nav class="container mx-auto p-4 flex justify-center items-center gap-x-12 hover:bg-gray-200">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/blog">Blog</RouterLink>
+        <RouterLink to="/portfolio">Portfolio</RouterLink>
     </nav>
   </header>
 </template>
+
+<style scoped>
+.menu-bar:hover {
+  background-color: #f0f0f0;
+}
+</style>
